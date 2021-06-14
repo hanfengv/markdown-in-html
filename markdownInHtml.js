ee295e9a6035a84c8d7c2ede6a996887e8a8e035
hanfeng <hanfengv@gmail.com>
2021-06-14 21:47:20 +0800

    斜体粗体

--- markdownInHtml.js
+++ markdownInHtml.js
@@ -1,6 +1,10 @@
-const testStr = "# H1\n###### H6\n\n## 段落 `p`\n\n两个`空行`中包含的文字是段落这是一个段落,它的前后都是空行\n\n## 换行 `br`\n如果要在段落中换行(即:插入 `<br>`), 在行尾打`两个空格`即可\n\n注意此行末尾有两个空格  \n我换行了\n\n## 引用 `blockquote`\n单行引用\n\n## 列表 `ul,ol`\n### 无序列表 `ul`\n使用 `+` `-` `*` 中任意一个符号,加上`空格`(空格推荐3个,方便嵌套).\n\n_   项目1\n-   项目二\n\n### 有序列表 `ol`\n使用数字和`.`加`空格` (空格推荐2个, 数字可以不按顺序)\n\n1.  项目1\n2.  项目2\n\n## 代码 `figure,pre,code`\n\n### 行内代码 `code`\n\n使用 ` 符号来包含代码\n\n### 普通代码块\n缩进四个空格\n\n// 注意: 每行开头都 缩进了四个空格\n$php_arr = array('hello' =&gt; 'world');\necho $php_arr;\n\n## 分隔线 `hr`\n推荐使用减号; `*` `-` `_` (至少连续使用3次)\n\n---\n***\n__________________\n\n<p>123<b>hello</b></p>\n\n<script>\n    console.log(123)\n</script>\n\n## 耳机目录\n\ndofjsdf\n\n# 一级\n\n测试\n";
-
 const PREFIX = "__MDHTML__";
+
+var trim = (str, ...chars) => {
+  const charStrs = chars.map((char) => `\\${char}`).join("|");
+  const reg = new RegExp(`^([${charStrs}])+|([${charStrs}])+$`, "g");
+  return str.replace(reg, "");
+};
 
 const getHtmlStr = () => document.body.innerHTML;
 
@@ -19,7 +23,7 @@ const mdhm = (htmlMarkup = "p", text = "", mdMarkup = "", matchs = []) => {
   };
 };
 
-// 标题
+// 标题（Headings） #
 const getHeadings = (str) => {
   const matchs = str.match(parseMap.get("headings").regExp);
   return matchs ? mdhm(`h${matchs[1]?.length - 1}`, matchs[2], matchs[1], matchs) : mdhm();
@@ -29,14 +33,39 @@ const parseHeadings = (str) => {
   return `<${mdh.htmlMarkup}>${mdh.text}</${mdh.htmlMarkup}>`;
 };
 
-// 加粗
+// TODO:: 段落（Paragraphs）
+// TODO:: 换行（Line Breaks）
+
+// 粗体（Bold）
 const getBold = (str) => {
   const matchs = str.match(parseMap.get("bold").regExp);
   return matchs ? mdhm("strong", matchs[2], matchs[1], matchs) : mdhm();
 };
 const parseBold = (str) => {
   const mdh = getBold(str);
-  mdh.matchs.forEach((item) => (str = str.replace(item, `<${mdh.htmlMarkup}>${item}</${mdh.htmlMarkup}>`)));
+  mdh.matchs.forEach((item) => (str = str.replace(item, `<${mdh.htmlMarkup}>${trim(item, "*", "_")}</${mdh.htmlMarkup}>`)));
+  return str;
+};
+
+// 斜体（Italic）
+const getItalic = (str) => {
+  const matchs = str.match(parseMap.get("italic").regExp);
+  return matchs ? mdhm("em", matchs[2], matchs[1], matchs) : mdhm();
+};
+const parseItalic = (str) => {
+  const mdh = getItalic(str);
+  mdh.matchs.forEach((item) => (str = str.replace(item, `<${mdh.htmlMarkup}>${trim(item, "*", "_")}</${mdh.htmlMarkup}>`)));
+  return str;
+};
+
+// 粗体（Bold）和斜体（Italic）
+const getBoldItalic = (str) => {
+  const matchs = str.match(parseMap.get("boldItalic").regExp);
+  return matchs ? mdhm("strong>em", matchs[2], matchs[1], matchs) : mdhm();
+};
+const parseBoldItalic = (str) => {
+  const mdh = getBoldItalic(str);
+  mdh.matchs.forEach((item) => (str = str.replace(item, `<strong><em>${trim(item, "*", "_")}</em></strong>`)));
   return str;
 };
 
@@ -70,10 +99,24 @@ var parseMap = new Map([
     },
   ],
   [
+    "boldItalic",
+    {
+      regExp: /([\*|_]{3})([^\*|_]*)\1/g,
+      method: parseBoldItalic,
+    },
+  ],
+  [
     "bold",
     {
       regExp: /([\*|_]{2})([^\*|_]*)\1/g,
       method: parseBold,
+    },
+  ],
+  [
+    "italic",
+    {
+      regExp: /([\*|_]{1})([^\*|_]*)\1/g,
+      method: parseItalic,
     },
   ],
   [
